57934cdc7922b17e52bff5b926693f0056e82218
devang-altf4 <maybedevang29@gmail.com>
2025-05-07 01:16:22 +0530

    build

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import '@/styles/globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'finance-tracker',
+  description: 'A finance tracker app built with Next.js and MongoDB',
+  generator: 'devang',
 }
 
 export default function RootLayout({
